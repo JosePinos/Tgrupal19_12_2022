58c7771f3f5cf43319f53a7e5f733bb1e0ea9c13
POO <miguel.carapaz@epn.edu.ec>
2022-12-19 11:52:15 -0500

    commit

---
+++ src/Obsidiana.java
@@ -0,0 +1,25 @@
+public class Obsidiana extends Cubito {
+    private String color;
+    private double dureza;
+    public Obsidiana(){
+        super();
+        this.color="Morado";
+        this.dureza=12.5;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getDureza() {
+        return dureza;
+    }
+
+    public void setDureza(double dureza) {
+        this.dureza = dureza;
+    }
+}
